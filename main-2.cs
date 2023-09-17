8033d47ec65fd68899eefd6289247fadf49c1531
Epson2 <142953187+Epson2@users.noreply.github.com>
2023-09-16 22:18:44 -0400

    Add files via upload

---
+++ main-2.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+
+class Program {
+  public static void Main (string[] args) {
+    WriteLine ("Lucinda Potter");
+    WriteLine ("6/24/1992");
+    WriteLine ("work 000-000-0101");
+    WriteLine ("cell 000-000-0189");
+  }
+}
